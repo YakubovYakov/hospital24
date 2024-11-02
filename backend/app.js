e8d9db7b5f276a46739a87880a8bb2471e3fd5c4
YakubovYakov <yaks1331@yandex.ru>
2024-11-02 19:28:00 +0300

    Fix app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 // app.js
 const express = require("express");
-const cors = require("cors"); // Импортируем cors
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const employersRoutes = require("./routes/employersRoutes");
@@ -16,8 +17,15 @@ app.use(
 
 app.use(express.json());
 
+// Роуты API
 app.use("/api/employers", employersRoutes);
 app.use("/api/departments", departmentsRoutes);
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
