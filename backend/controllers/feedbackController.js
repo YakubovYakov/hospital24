c66674b52ef545c272b998c2c1ba863c9830d0e4
YakubovYakov <yaks1331@yandex.ru>
2024-11-28 11:33:23 +0300

    Fix feedbacks controllers

--- backend/controllers/feedbackController.js
+++ backend/controllers/feedbackController.js
@@ -5,19 +5,17 @@ const getFeedbacks = async (req, res) => {
   try {
     const result = await pool.query(`
 		SELECT 
-		f.id,
-		f.feedback_source_id,
-		f.emp_id,
-		f.full_text,
-		f.author,
-		to_char(f.feedback_date, 'DD.MM.YYYY') AS feedback_date,
-		e.full_name AS employee_name,
-		fs.name AS feedback_source_name,
-		fs.path AS icon_path 
-		FROM feedback_emp f
-		LEFT JOIN employers e ON f.emp_id = e.id
-		LEFT JOIN feedback_source fs ON f.feedback_source_id = fs.id  
-		ORDER BY f.feedback_date DESC;
+        f.id,
+        f.feedback_source_id,
+        f.emp_id,
+        f.full_text,
+        f.author,
+        to_char(f.feedback_date, 'DD.MM.YYYY') AS feedback_date,
+        fs.name AS feedback_source_name,  -- Название источника
+        fs.path AS icon_path              -- Иконка источника
+      FROM feedback_emp f
+      LEFT JOIN feedback_source fs ON f.feedback_source_id = fs.id
+      ORDER BY f.feedback_date DESC;
     `);
     res.json(result.rows);
   } catch (error) {
@@ -32,20 +30,20 @@ const getDepartmentFeedbacks = async (req, res) => {
     const result = await pool.query(
       `
       SELECT 
-    f.id,
-    f.feedback_source_id,
-    f.dept_id,
-    f.full_text,
-    f.author,
-    to_char(f.feedback_date, 'DD.MM.YYYY') AS feedback_date,
-    fs.name AS feedback_source_name,
-    fs.path AS icon_path,
-    d.name AS department_name  -- Добавляем название отделения
-		FROM feedback_dept f
-		LEFT JOIN feedback_source fs ON f.feedback_source_id = fs.id
-		LEFT JOIN dept d ON f.dept_id = d.id  -- Присоединяем таблицу с отделениями
-		WHERE f.dept_id = $1
-		ORDER BY f.feedback_date DESC;
+        f.id,
+        f.feedback_source_id,
+        f.dept_id,
+        f.full_text,
+        f.author,
+        to_char(f.feedback_date, 'DD.MM.YYYY') AS feedback_date,
+        fs.name AS feedback_source_name,  -- Название источника
+        fs.path AS icon_path,             -- Иконка источника
+        d.name AS department_name         -- Название отделения
+      FROM feedback_dept f
+      LEFT JOIN feedback_source fs ON f.feedback_source_id = fs.id
+      LEFT JOIN dept d ON f.dept_id = d.id
+      WHERE f.dept_id = $1
+      ORDER BY f.feedback_date DESC;
       `,
       [deptId]
     );
